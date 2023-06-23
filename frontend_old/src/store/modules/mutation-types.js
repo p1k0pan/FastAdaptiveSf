a075c1deb9cd7c182af6c387f1b5661e9c3e7a53
Adrian <lindloff@uni-bremen.de>
2023-06-23 21:53:10 +0200

    changing and deleting files

--- frontend_old/src/store/modules/mutation-types.js
+++
@@ -1,8 +0,0 @@
-/**
- * This file shows all available mutations
- * in all our modules.
- * The pattern is explained here: https://vuex.vuejs.org/guide/mutations.html#using-constants-for-mutation-types
- */
-
-//For index.js in auth
-
