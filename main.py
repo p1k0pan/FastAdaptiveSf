4bf8c6c38014c911dd10d66fa79b74d9ac78168f
p1k0pan <645758634@qq.com>
2023-06-26 16:00:28 +0200

    login to create token | token verification

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from contextlib import asynccontextmanager
 import pandas as pd
 from modules import controller as con
+from modules import authorization as auth
 
 from sqlalchemy import text
 from db import config, schema, crud, model
 from sqlalchemy.orm import Session
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
 from gradio_client import Client
+from typing import Union
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 
 corpus_embeddings = None # model from main dataset
 client=None
@@ -21,6 +25,8 @@ def get_db():
         yield db
     finally:
         db.close()
+
+db_session = Depends(get_db)
 
 app = FastAPI()
 
@@ -84,19 +90,50 @@ async def search_query_history(query:str=""):
 
 
 @app.get('/user')
-async def get_user(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def get_user(skip: int = 0, limit: int = 100, db: Session =db_session):
     _users = crud.get_all_user(db, skip, limit)
     return schema.Response(status="Ok", code="200", message="Success fetch all data", result=_users)
 
 @app.post("/user")
-async def create_user(request: schema.UserSchema, db: Session = Depends(get_db)):
+async def create_user(request: schema.UserSchema, db: Session =db_session): 
     _user = crud.create_user(db, user=request)
+    # detect exception from input
     return schema.Response(status="Ok",
-                    code="200",
+                    code="201",
                     message="User created successfully", result=_user)
 
 @app.patch("/user")
-async def update_histories(request: schema.UserSchema, db: Session = Depends(get_db)):
+async def update_histories(request: schema.UserSchema, db: Session =db_session):
+    # detect upload_urls exception
     _user = crud.update_history(db, user_name=request.user_name, upload_urls=request.upload_urls)
     return schema.Response(status="Ok", code="200", message="Success update data", result=_user)
+
+@app.post("/login")
+async def login_to_create_token( user: schema.UserSchema, db: Session =db_session):
+    _user = crud.get_user(db, user.user_name)
+    if not _user:
+        return schema.Response(status="Bad Request",
+                    code="400",
+                    message="Invalid user name or user not found", result=None)
+    else:
+        if _user.password !=user.password:
+            return schema.Response(status="Bad Request",
+                        code="400",
+                        message="Invalid password", result=None)
+        else:
+            # assign a token
+            token = auth.create_token(_user.user_name)
+
+            return schema.Response(status="Ok",
+                        code="200",
+                        message="login success", result=token)
+
+@app.get("/token_verify")
+async def token_verif(request:Request):
+    try:
+        token = request.headers['authorization']
+        status, code, msg, result = auth.token_decode(token)
+        return schema.Response(status=status, code=code, message=msg, result=result)
+    except KeyError:
+        return schema.Response(status="Failed", code='404', message="Token not found in Header", result=None)
 
