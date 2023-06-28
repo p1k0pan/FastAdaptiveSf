e5f5d0b803eda9414a1d8e8b2d69e47b058508a2
p1k0pan <645758634@qq.com>
2023-06-28 15:00:31 +0200

    Capable to refresh token

--- modules/authorization.py
+++ modules/authorization.py
@@ -2,39 +2,58 @@ import jwt
 
 from datetime import datetime, timedelta
 
-key = "aDaPtIvE_sToRyFiNdEr_23"
-algorithm = "HS256"
+KEY = "aDaPtIvE_sToRyFiNdEr_23"
+ALGORITHM = "HS256"
+REFRESH_ACESS_MINUTE = 20
 
 
-def create_token(user_name:str):
-    expiration = datetime.utcnow() + timedelta(hours=1)
+def create_token(user_name:str, minute:int):
+    expiration = datetime.utcnow() + timedelta(minutes=minute)
     payload  ={"user_name":user_name, "exp": expiration}
 
-    encoded = jwt.encode(payload, key, algorithm=algorithm)
+    encoded = jwt.encode(payload, KEY, algorithm=ALGORITHM)
 
     # result = {"usaer_name": user.user_name, "header": request.headers['authorization']}
     return encoded
 
-def token_decode(token:str):
+def token_validation(token:str, refresh=False):
     try:
         # Verify the token's signature
-        payload = jwt.decode(token, key, algorithms = [algorithm])
+        payload = jwt.decode(token, KEY, algorithms = [ALGORITHM])
 
-        # Check the token's expiration time
-        current_time = datetime.utcnow()
-        expiration_time = datetime.fromtimestamp(payload["exp"])
-
-        if current_time > expiration_time:
-            # Token has expired
-            return ["Ok", '203', "Token is expired", None]
+        if refresh:
+            # Token is valid, generate new one
+            return ["Ok", '201', "Token is refreshed", payload["user_name"]]
         else:
-            # Token is valid
             return ["Ok", '200', "Token is valid", payload["user_name"]]
+
 
     except jwt.ExpiredSignatureError as e:
         # Signature verification failed
-        print(e)
-        return ['Failed',"400", 'Signature verification failed. Token has expired.', None]
+
+        unverified_data = jwt.decode(token, KEY, algorithms = [ALGORITHM],options={"verify_signature": False})
+
+        current_time = datetime.utcnow()
+        expiration_time = datetime.fromtimestamp(unverified_data["exp"])
+
+        if refresh:
+            if current_time > expiration_time:
+                # refresh Token has expired
+                return ['Failed',"400", 'Signature verification failed. Token has expired. Login agian', None]
+            else:
+                # pass the access token instead of refresh token
+                return ['Failed',"400", 'Please pass the refresh token', None]
+
+        else:
+            buffer_time = timedelta(minutes=REFRESH_ACESS_MINUTE)
+
+            # Token has expired
+            if current_time - expiration_time < buffer_time:
+                # in refresh time
+                return ["Failed", '401', "Token can be refresh", None]
+            else:
+                # not in refresh time
+                return ['Failed',"400", 'Signature verification failed. Token has expired. Login agian', None]
     except jwt.InvalidSignatureError as e:
         # Signature verification failed
         print(e)
