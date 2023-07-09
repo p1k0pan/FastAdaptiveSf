4bf3b9fd013716d008005fdfb597275de65f2f73
Loffl <al.lindloff01@gmail.com>
2023-07-09 22:46:23 +0200

    register and login should work; backend error

--- main.py
+++ main.py
@@ -48,10 +48,17 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
 # NEW
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
